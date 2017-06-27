9c1820ae10eac8829e92572cd9bac6744ba9ca90
Ioan Loosley <legit.ioangogo@gmail.com>
2017-06-27 14:09:12 +0100

    Added workaround for the serverside timeout

--- bluempd/bluempd.py
+++ bluempd/bluempd.py
@@ -6,15 +6,9 @@ print(dev)
 
 client = MPDClient()
 client.connect("localhost", 6600)
-
+client.timeout = 10
 print("Connected to the MPD Server")
-
-def getplaystate():
-   state = client.status()['state']
-   if state == "play":
-      return False
-   else:
-      return True
+client.idle
 
 
 commandmap = {
@@ -24,26 +18,20 @@ commandmap = {
 200:client.play
 }
 
-playstate = getplaystate()
 
 def mpdcontrol(code):
+    client.noidle
     global playstate
+    print(code.scancode)
     if code.keystate == 1:
-      if code.scancode == 200 or code.scancode == 201:
-          mpdcmd = commandmap[201]
-          int_play = int(bool(playstate))
-          mpdcmd(int_play)
-          playstate = getplaystate()
       mpdcmd = commandmap[code.scancode]
       mpdcmd()
       time.sleep(4)
+    client.idle
 
 while True:
-  oldplay = playstate
-  playstate = getplaystate()
-  if playstate != oldplay: print("MPD is in the " + client.status()['state'] + " State")
+  client.ping()
   for event in dev.read_loop():
        if event.type == ecodes.EV_KEY:
             data = categorize(event)
             mpdcontrol(data)
-#           print(data)
